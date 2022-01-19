8454783097309e23227a1fdc29ac183405b0795c
oliv-sok <oli.sokolowski@gmail.com>
2022-01-19 08:32:04 +0100

    2222222222222222222222

---
+++ src/main/java/com/zespol11/programowanienzespolowe/order/Order.java
@@ -0,0 +1,4 @@
+package com.zespol11.programowanienzespolowe.order;
+
+public class Order {
+}
---
+++ src/main/java/com/zespol11/programowanienzespolowe/order/OrderRepository.java
@@ -0,0 +1,2 @@
+package com.zespol11.programowanienzespolowe.order;public interface OrderRepository {
+}
