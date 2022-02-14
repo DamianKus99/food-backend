13bb23a0d6dec341ee6cf7426ba89c93634c3623
oliv-sok <oli.sokolowski@gmail.com>
2022-02-14 09:36:38 +0100

    test zamowienia v3

--- src/main/java/com/zespol11/programowanienzespolowe/food/FoodItem.java
+++ src/main/java/com/zespol11/programowanienzespolowe/food/FoodItem.java
@@ -3,6 +3,7 @@ package com.zespol11.programowanienzespolowe.food;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @Setter
 @NoArgsConstructor
 @Entity
+@CrossOrigin("*")
 public class FoodItem {
 
     @Id
--- src/main/java/com/zespol11/programowanienzespolowe/order/OrderController.java
+++ src/main/java/com/zespol11/programowanienzespolowe/order/OrderController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/order")
+@CrossOrigin("*")
 public class OrderController {
 
     private final OrderMasterService orderMasterService;
